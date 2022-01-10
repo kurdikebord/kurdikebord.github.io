904cb9f9a068e44c7d3eca3add36f44d1d472ffb
kurdikeyboard <gorran.kurdi@gmail.com>
2022-01-10 15:44:32 +0300

    .

--- Quran/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header } from "./header/Header";
-export { default as Cards } from "./cards/Cards";
-export { default as MediaPlayer } from "./player/MediaPlayer";
-export { default as TemporaryDrawer } from "./drawer/TemporaryDrawer";
