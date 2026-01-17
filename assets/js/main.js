fcded4abd77a620a5e9763d611852e20956f1860
KuRa KuЯd <gorran.kurdi@gmail.com>
2026-01-18 02:37:44 +0300

    Update Contact Address on All Pages..

--- assets/js/main.js
+++ assets/js/main.js
@@ -210,32 +210,3 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
-
-
-
-const themeStitcher = document.getElementById("themingSwitcher");
-const isSystemThemeSetToDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-// set toggler position based on system theme
-if (isSystemThemeSetToDark) {
-  themeStitcher.checked = true;
-}
-
-// add listener to theme toggler
-themeStitcher.addEventListener("change", (e) => {
-  toggleTheme(e.target.checked);
-});
-
-const toggleTheme = (isChecked) => {
-  const theme = isChecked ? "dark" : "light";
-
-  document.documentElement.dataset.mdbTheme = theme;
-}
-
-// add listener to toggle theme with Shift + D
-document.addEventListener("keydown", (e) => {
-  if (e.shiftKey && e.key === "D") {
-    themeStitcher.checked = !themeStitcher.checked;
-    toggleTheme(themeStitcher.checked);
-  }
-});
